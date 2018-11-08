aaa2766eb71cd179b73a67b855b78be5869423e1
Aljaz Kern <aljazkern@aljazs-macbook-pro.local>
2018-11-08 19:59:31 +0100

    Initial Commit

---
+++ TodoMap/TodoListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  TodoMap
+//
+//  Created by Aljaz Kern on 07/11/2018.
+//  Copyright © 2018 Aljaz Kern. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let testItemArray = ["And do somethin else", "Learn for History test", "Finish report for school"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testItemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        cell.textLabel?.text = testItemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: true)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+         tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+
+}
+
