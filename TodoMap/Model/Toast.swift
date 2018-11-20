b2b6dd09321b1b8f4d542ac4c319a522b61392fd
aljaz90 <35540285+aljaz90@users.noreply.github.com>
2018-11-20 14:07:45 +0100

    Added Toast class
    Added Toast class

---
+++ TodoMap/Model/Toast.swift
@@ -0,0 +1,30 @@
+//
+//  Toast.swift
+//  TodoMap
+//
+//  Created by Aljaz Kern on 20/11/2018.
+//  Copyright © 2018 Aljaz Kern. All rights reserved.
+//
+
+import Foundation
+
+class Toast {
+	
+	init(message : String, backgroundColor: UIColor = UIColor.black, color: UIColor = UIColor.white, time: Double = 4.0){
+		let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: 15, width: 150, height: 35))
+        toastLabel.backgroundColor = backgroundColor.withAlphaComponent(0.6)
+        toastLabel.textColor = color
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 16.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: time, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+	}
+}
