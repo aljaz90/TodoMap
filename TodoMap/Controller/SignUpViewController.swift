cd46be31a59d79597b23d7b70ff49bb6ba7ff3fd
Aljaz Kern <aljazkern@aljazs-macbook-pro.local>
2018-12-04 21:41:27 +0100

    Added even more styling to Sign Up VC

--- TodoMap/Controller/SignUpViewController.swift
+++ TodoMap/Controller/SignUpViewController.swift
@@ -18,16 +18,17 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addLineToView(view: emailField, position: .LINE_POSITION_BOTTOM, color: UIColor.white, width: 2.0)
+        addLineToView(view: passwordField, position: .LINE_POSITION_BOTTOM, color: UIColor.white, width: 2.0)
+        let gradient: CAGradientLayer = CAGradientLayer()
 
-//        let gradient: CAGradientLayer = CAGradientLayer()
-//
-//        gradient.colors = [UIColor.randomFlat.cgColor, UIColor.randomFlat.cgColor]
-//        gradient.locations = [0.0 , 1.0]
-//        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
-//        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
-//        gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-//
-//        self.view.layer.insertSublayer(gradient, at: 0)
+        gradient.colors = [UIColor.randomFlat.cgColor, UIColor.randomFlat.cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+
+        self.view.layer.insertSublayer(gradient, at: 0)
     }
     
     
@@ -48,6 +49,30 @@ class SignUpViewController: UIViewController {
 //        }
     }
     
+    enum LINE_POSITION {
+        case LINE_POSITION_TOP
+        case LINE_POSITION_BOTTOM
+    }
+    
+    func addLineToView(view : UIView, position : LINE_POSITION, color: UIColor, width: Double) {
+        let lineView = UIView()
+        lineView.backgroundColor = color
+        lineView.translatesAutoresizingMaskIntoConstraints = false // This is important!
+        view.addSubview(lineView)
+        
+        let metrics = ["width" : NSNumber(value: width)]
+        let views = ["lineView" : lineView]
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[lineView]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+        
+        switch position {
+        case .LINE_POSITION_TOP:
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[lineView(width)]", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        case .LINE_POSITION_BOTTOM:
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[lineView(width)]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        }
+    }
     
     
     
